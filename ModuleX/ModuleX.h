ad62291a96edafa5229ef0ca248895856b294739
NGUYEN CHI CONG <congnc.if@gmail.com>
2018-11-15 14:32:21 +0700

    init

---
+++ ModuleX/ModuleX.h
@@ -0,0 +1,19 @@
+//
+//  ModuleX.h
+//  ModuleX
+//
+//  Created by FOLY on 11/15/18.
+//  Copyright © 2018 [iF] Solution. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModuleX.
+FOUNDATION_EXPORT double ModuleXVersionNumber;
+
+//! Project version string for ModuleX.
+FOUNDATION_EXPORT const unsigned char ModuleXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModuleX/PublicHeader.h>
+
+
